773733e966c0b7383a0f4815b0fa55c26cd59618
yasith <it19966922@my.sliit.lk>
2022-10-17 09:27:39 +0530

    create fuel station controller

--- Controllers/FuelStationController.js
+++ Controllers/FuelStationController.js
@@ -1,11 +1,4 @@
+import FuelStation from '../Models/FuelStation.js';
 
 
-const getVehicleCountByType = async (req, res) => {
-    try {
-        const vehicleType = req.query.vehicleType;
-        const vehicleCount = await Vehicle.countDocuments({ vehicleType: vehicleType });
-        res.status(200).json({ vehicleCount: vehicleCount });
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
+
