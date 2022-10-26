6c926da2e0eb957a6056a6fd2f05d384847466da
yasith <it19966922@my.sliit.lk>
2022-10-26 13:30:21 +0530

    create updateFuelStatusByName method

--- Routes/FuelStatusRouter.js
+++ Routes/FuelStatusRouter.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { addFueStatus } = require("../controllers/FuelStatusController");
+const {updateFuelStatusByName} = require("../Controllers/FuelStatusController");
 
 router.route("/").post(addFueStatus);
+router.route("/update").post(updateFuelStatusByName);
 
 module.exports = router;
