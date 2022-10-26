0fe7a1acf17b479169c6bbb54bfc8023c7c2465e
yasith <it19966922@my.sliit.lk>
2022-10-26 13:26:20 +0530

    create updateFuelStatusByName method

--- Controllers/FuelStatusController.js
+++ Controllers/FuelStatusController.js
@@ -18,3 +18,24 @@ exports.addFueStatus = async (req, res) => {
     res.status(409).json({ message: error.message });
   }
 };
+
+/**
+ * @description - This function is used to update the fuel status by fuel station name
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
+exports.updateFuelStatusByName = async (req, res) => {
+    let fuelStatus = req.body;
+    try {
+        const updatedFuelStatus = await FuelStatus.findOneAndUpdate(
+        { fuelStation: fuelStatus.name },
+        fuelStatus,
+        { new: true }
+        );
+        res.status(200).json(updatedFuelStatus);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+
+}
